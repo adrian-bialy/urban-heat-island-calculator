5ced336de6f48f8b9c3d7253839d4b79c27f6b31
adrian.bialy <adrian.bialy@airlab.me>
2024-10-06 13:03:34 +0200

    increase timeout

--- src/services/external_api.py
+++ src/services/external_api.py
@@ -5,7 +5,7 @@ from flask import current_app
 
 def send_to_external_backend(url, data):
     try:
-        response = requests.post(url, json=data, timeout=5)
+        response = requests.post(url, json=data, timeout=10000)
         response.raise_for_status()
         return True, response.text
     except requests.exceptions.RequestException as e:
